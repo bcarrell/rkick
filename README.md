ced7b3fc5fdf4936e74ffc6599af73a14a70b828
Brandon Carrell <brandoncarrell@gmail.com>
2014-08-20 16:20:29 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# rkick
+
+    npm install rkick
+
+rkick is a helper to solve a common problem with remote development workflows (especially with vim).  Your
+development environment works great on your local machine, but when you need to work off a remote host, it falls apart.
+SSHFS can be frustratingly slow.  rkick is essentially a wrapper around rsync.
+
+You can do two things with rkick:  `--push <dest>` and `--pull <dest>`.  *Both work with your current directory.*
+
+To start syncing your current directory with your remote host: `rkick --push dev:~/path/to/dir/`.  `rkick` will watch everything
+and rsync when files change.
+
+To update your current working directory from your remote host: `rkick --pull dev:~/path/to/dir/`.  Like rsync, if you want the
+contents only, don't forget your trailing slash.
+
+### License
+
+MIT.
---
+++ rkick.js
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+
+var gaze  = require('gaze');
+var Rsync = require('rsync');
+var _     = require('lodash');
+var cli   = require('commander');
+
+var dir = process.cwd() + '/';
+var form = '[server:/path/to/destination]';
+
+cli
+.version('0.0.1')
+.option('--pull <path>', 'update this directory from remote path ' + form)
+.option('--push <path>', 'update the remote destination ' + form)
+.parse(process.argv);
+
+if ((!cli.push && !cli.pull) || (cli.push && cli.pull)) {
+  console.log('You must either push or pull.');
+  cli.help();
+  process.exit(1);
+}
+
+if (cli.pull) {
+  rsync(cli.pull, dir)();
+}
+
+if (cli.push) {
+  var dest = cli.push;
+  var sync = rsync(dir, dest);
+
+  sync();
+
+  gaze('**/*', function(err, watcher) {
+    if (err) throw err;
+
+    watcher.on('all', _.debounce(function() {
+      console.log('Change detected!');
+      sync();
+    }, 200));
+
+  });
+}
+
+function rsync(src, dest) {
+  var rsync = new Rsync()
+    .flags('az')
+    .set('delete')
+    .source(src)
+    .destination(dest);
+
+  return function() {
+    console.log(src + ' => ' + dest + '\n');
+    console.log('Syncing...');
+    rsync.execute(function(err) {
+      if (err) throw err;
+
+      console.log('Sync complete!');
+    });
+  };
+}
